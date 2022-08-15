1894ad2b0a1e1a13fa37849eee97c5592f899056
prabath1998 <prabhathudayanga1998@gmail.com>
2022-08-15 15:52:24 +0530

    Initial commit

---
+++ src/main/java/com/test/oauthgoogle/OAuthGoogleApplication.java
@@ -0,0 +1,13 @@
+package com.test.oauthgoogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthGoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthGoogleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/oauthgoogle/OAuthGoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.oauthgoogle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthGoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
